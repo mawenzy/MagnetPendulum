061290c7f5721d18e7b0d05b5bcd635a5a00452b
mawenzy <>
2018-06-05 23:21:49 +0200

    added source code

---
+++ src/magneticPendulum.c
@@ -0,0 +1,351 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define RADIUS 0.5 // distance magnet - origin
+#define h 0.02     // step size in calculation of differential equation
+#define Z 0.25     // hight of the pendulum
+
+/*
+*   Function: getXForce
+*   ------------------------------------
+*   calculates the x-component of the force the magnets apply to the pendulum
+*
+*   (xalt , yalt):  position of the pendulum in the x-y plane
+*   F:              Factor to change the strength of the force
+*   N:              Amount of magnets, magnets are placed equally around a
+*                       circle of radius 'RADIUS'
+*
+*   returns:        x - component of the force
+*/
+double getXForce(double xalt, double yalt, double F, int N)
+{
+    int i;
+    double force = 0;
+    //iterate over all magnets
+    for (i = 0; i < N; i++)
+    {
+        double magX = RADIUS * cos(2 * M_PI * i / N); //x coordinate of magnet
+        double magY = RADIUS * sin(2 * M_PI * i / N); //y coordinate of magnet
+
+        double dis = sqrt(Z * Z + (xalt - magX) * (xalt - magX) + (yalt - magY) * (yalt - magY));
+
+        force = force + (xalt - magX) / (dis * dis * dis);
+    }
+    return F * force;
+}
+
+/*
+*   Function: getYForce
+*   ------------------------------------
+*   calculates the y-component of the force the magnets apply to the pendulum
+*
+*   (xalt , yalt):  position of the pendulum in the x-y plane
+*   F:              Factor to change the strength of the force
+*   N:              Amount of magnets, magnets are placed equally around a
+*                       circle of radius 'RADIUS'
+*
+*   returns:        y - component of the force
+*/
+double getYForce(double xalt, double yalt, double F, int N)
+{
+    int i;
+    double force = 0;
+    //iterate over all magnets
+    for (i = 0; i < N; i++)
+    {
+        double magX = RADIUS * cos(2 * M_PI * i / N); //x coordinate of magnet
+        double magY = RADIUS * sin(2 * M_PI * i / N); //y coordinate of magnet
+
+        double dis = sqrt(Z * Z + (xalt - magX) * (xalt - magX) + (yalt - magY) * (yalt - magY));
+
+        force = force + (yalt - magY) / (dis * dis * dis);
+    }
+    return F * force;
+}
+
+/*
+*   Function: getColour
+*   -------------------------------
+*   evaluates the position of the pendulum, decides in which circle sector it is.
+*
+*   (x, y):         position in the x-y plane
+*   N:              Amount of sectors, sectors are spaced with equal size around the origin
+*
+*   returns:        int corresponding to the magnet
+*/
+int getColour(double x, double y, int N)
+{
+    double angle = atan2(y, x);
+    //special treatment of first sector, easy to calculate this way
+    if (fabs(angle) < M_PI / N)
+    {
+        return 1;
+    }
+    //change to an angle between 0 and 2pi
+    /*
+        NOTE:
+        To work with the angles in an easy way, you want 
+            atan2( sin(t), cos(t)) = t for t in [0:2*pi],
+        the next if statement fixes the result from the atan2
+        function in a way making this equation true for all angles
+    */
+    if (angle < 0)
+    {
+        angle += 2 * M_PI;
+    }
+    int i;
+    //iterator over the rest of the sectors
+    for (i = 1; i < N; i++)
+    {
+        //test if the pendulum is in the sector
+        if (angle >= 2 * M_PI * i / N - M_PI / N && angle <= 2 * M_PI * (i + 1) / N - M_PI / N)
+        {
+            return i + 1;
+        }
+    }
+    return 0;
+}
+
+/*
+*   Function: calcDiff
+*   ----------------------------------------------
+*   calculates the movement of the pendulum until it has stopped
+*
+*   (xalt, yalt):       starting position
+*   F:                  coeffizient to the force
+*   N:                  amount of magnets
+*   Y:                  strength of drag
+*   K:                  spring constant
+*
+*   returns:            int correspoding to the sector of the end position
+*                           of the pendulum
+*/
+int calcDiff(double xalt, double yalt, double F, int N, double Y, double K)
+{
+    double vxalt = 0, vyalt = 0; // start values for speed
+
+    int amount = 0;
+    int wasMoving = 0; //keeps track if the pendulum started moving, to start testing the stop condition (low cinetic energy) sooner
+
+    //calculates the movement of the pendulum
+    while (amount <= 5000)
+    {
+        //calculate acceleration
+        double axalt = -K * xalt - Y * vxalt - getXForce(xalt, yalt, F, N);
+        double ayalt = -K * yalt - Y * vyalt - getYForce(xalt, yalt, F, N);
+
+        //get approximated position values using taylor expansion to the second term
+        double xneu = xalt + h * vxalt + 0.5 * h * h * axalt;
+        double yneu = yalt + h * vyalt + 0.5 * h * h * ayalt;
+
+        //calculate new speed values
+        double vxneu = vxalt + h * axalt;
+        double vyneu = vyalt + h * ayalt;
+
+        //shift position values
+        xalt = xneu;
+        yalt = yneu;
+        vxalt = vxneu;
+        vyalt = vyneu;
+
+        //test if pendulum has started moving
+        if (vxalt * vxalt + vyalt * vyalt > 0.1)
+        {
+            wasMoving = 1;
+        }
+
+        amount++;
+        // at least 1000 iterations before testing the stop condition
+        if (amount > 1000 || wasMoving == 1)
+        {
+            //energy small > pendulum is supposed to be in the final sector
+            if (vxalt * vxalt + vyalt * vyalt < 0.01)
+            {
+                return getColour(xalt, yalt, N);
+            }
+        }
+    }
+    return getColour(xalt, yalt, N);
+}
+
+int main(int argc, char **argv)
+{
+    double Y; //drag coefficient
+    double K; //spring constant
+    double F; //force of magnets
+    int N;    //amount of magnets
+    double ZOOM;
+    double STEP = 0.05; // 0.005 -> 500x500
+    char *outputFile = "magnet.dat";
+
+    //variables for reading in the input
+    int qflag = 0, hflag = 0, oflag = 0, mflag = 0, zflag = 0, uflag = 0, fflag = 0, yflag = 0, kflag = 0;
+    char *amountMagnets, *zoomString, *force, *yString, *kString;
+
+    extern char *optarg;
+    extern int optind;
+    int c, err = 0;
+    static char usage[] = "usage: magnets.exe [-qhu] [-o outputFile] [-f force] [-m magnets] [-z zoom] [-y drag] [-k spring_constant]\n";
+
+    //o:outputFile m:magnets z:zoom f:force q_quiet h_help u_usage y:drag k:spring_constant o:outputfile
+    while ((c = getopt(argc, argv, "o:m:z:f:qhuy:k:")) != -1)
+    {
+        switch (c)
+        {
+        case 'y':
+            yflag = 1;
+            yString = optarg;
+            break;
+        case 'k':
+            kflag = 1;
+            kString = optarg;
+            break;
+        case 'q':
+            qflag = 1;
+            break;
+        case 'h':
+            hflag = 1;
+            break;
+        case 'o':
+            oflag = 1;
+            outputFile = optarg;
+            break;
+        case 'm':
+            mflag = 1;
+            amountMagnets = optarg;
+            break;
+        case 'f':
+            fflag = 1;
+            force = optarg;
+            break;
+        case 'u':
+            uflag = 1;
+            break;
+        case 'z':
+            zflag = 1;
+            zoomString = optarg;
+        case '?':
+            err = 1;
+            break;
+        }
+    }
+
+    //inpret input arguments
+    //set amount of magnets
+    if (mflag == 1)
+    {
+        N = atoi(amountMagnets);
+    }
+    else
+    {
+        N = 3;
+    }
+    //set zoom level
+    if (zflag)
+    {
+        ZOOM = atof(zoomString);
+    }
+    else
+    {
+        ZOOM = 1.25;
+    }
+    //set force
+    if (fflag)
+    {
+        F = atof(force);
+    }
+    else
+    {
+        F = 0.4;
+    }
+    //set drag
+    if (yflag == 1)
+    {
+        Y = atof(yString);
+    }
+    else
+    {
+        Y = 0.2;
+    }
+    //set federkonstante
+    if (kflag == 1)
+    {
+        K = atof(kString);
+    }
+    else
+    {
+        K = 0.3;
+    }
+    //print usage
+    if (err == 1 || uflag == 1 || hflag == 1)
+    {
+        printf(usage);
+        return 0;
+    }
+
+    //terminate program for false parameters, one magnet only would produce a one color image
+    if (N == 1)
+    {
+        printf("............................................________\n....................................,.-''...................``~.,\n.............................,.-'...................................'-.,\n.........................,/...............................................':,\n.....................,?......................................................,\n.................../...........................................................,}\n................./......................................................,:`^`..}\n.............../...................................................,:'........./\n..............?.....__.........................................:`.........../\n............./__.(.....'~-,_..............................,:`........../\n.........../(_....'~,_........'~,_....................,:`........_/\n..........{.._$;_......'=,_.......'-,_.......,.-~-,},.~';/....}\n...........((.....*~_.......'=-._......';,,./`..../'............../\n...,,,___.`~,......'~.,....................`.....}............../\n............(....`=-,,.......`........................(......;_,,-'\n............/.`~,......`-...................................../\n.............`~.*-,.....................................|,./.....,__\n,,_..........}.>-._...................................|..............`=~-,\n.....`=~-,__......`,.................................\n...................`=~-,,.,...............................\n................................`:,,...........................`..............__\n.....................................`=-,...................,%%`>--==``\n........................................_..........._,-%%.......`\n...................................,");
+        return 0;
+    }
+
+    //approx amount of pixels
+    int PIXELS = (int)(2 * ZOOM / STEP) * (2 * ZOOM / STEP);
+
+    //print info:
+    if (qflag == 0)
+    {
+        printf("Amount of data points to calculate: %d\n", PIXELS);
+        printf("Amount of magnets: %d, Force: %lg\n", N, F);
+        printf("ZOOM: %lg\n", ZOOM);
+        printf("Output file: %s\n", outputFile);
+    }
+
+    int current_pixels = 0;
+    FILE *file = fopen(outputFile, "w");
+    // go through a x-y grid 
+    for (double x = -ZOOM; x <= ZOOM + STEP; x += STEP)
+    {
+        for (double y = -ZOOM; y <= ZOOM + STEP; y += STEP)
+        {
+
+            //file output
+            fprintf(file, "%d ", calcDiff(x, y, F, N, Y, K));
+
+            //command line progress bar
+            current_pixels++;
+            if (current_pixels % (PIXELS / 100) == 0)
+            {
+                //update progress bar
+                int percent = (int)((double)current_pixels / (double)PIXELS * 100);
+                int n;
+                printf("\rprogress: |");
+                for (n = 0; n < 40; n++)
+                {
+                    if (percent > n * 2.5)
+                    {
+                        printf("#");
+                    }
+                    else
+                    {
+                        printf("-");
+                    }
+                }
+                printf("| %d %%", percent);
+            }
+        }
+        fprintf(file, "\n"); // newline after each row
+    }
+    printf("\rprogress: |########################################| 100 %%\n");
+
+    int close = fclose(file);
+    if (close != 0)
+    {
+        printf("Error in closing the file");
+        return 0;
+    }
+    return 0;
+}
